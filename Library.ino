Mammad900/Gui900

commit ae2758dcc61b507730abd8a962742543098b45e1

added 2 slider modifier functions
changeSliderEnabled: enabled
changeSliderVisible: visible

Library.ino
@@ -1296,6 +1296,24 @@ int Centre(int Length, int wid, int Left, int CHwid) {
                 drawSlider(page,i);
         }
     }
+    void changeSliderEnabled(int page,int i, bool val){
+        if(slider_enabled[page][i]!=val){
+            slider_enabled[page][i]=val;
+            if(CurrentPage=page)
+                drawSlider(page,i);
+        }
+    }
+    void changeSliderVisible(int page,int i, bool val){
+        if(slider_visible[page][i]!=val){
+            slider_visible[page][i]=val;
+            if(CurrentPage=page){
+                if(val)
+                    drawSlider(page,i);
+                else
+                    undrawSlider(page,i);
+            }
+        }
+    }
 #endif
 void navigatePage( int page , int transition){ // Navigates to another page
     HCT

commit 47c5a2142d8330f2cf3b920162c5d767ebb83a57

Fixed 2 compiler errors caused by ae2758d

Library.ino
@@ -1112,7 +1112,7 @@ int Centre(int Length, int wid, int Left, int CHwid) {
                 }
                 slider_width[page][i] = val; // Assign the value
                 slider_value[page][i] =
-                    map(value, min, max, 0,
+                    map(value, slider_min[page][i], slider_max[page][i], 0,
                         slider_width[page][i] - slider_thumb_width[page][i]);
                 drawSlider(page, i); // Draw the new slider
             }
@@ -1120,7 +1120,7 @@ int Centre(int Length, int wid, int Left, int CHwid) {
               slider_width[page][i] =
                   val; // Just assign the var if it is invisible
               slider_value[page][i] =
-                  map(value, min, max, 0,
+                  map(value, slider_min[page][i], slider_max[page][i], 0,
                       slider_width[page][i] - slider_thumb_width[page][i]);
             }
         }